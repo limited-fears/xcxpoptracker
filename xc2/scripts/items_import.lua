5dcf506c2866ef175e3004c3837bc6fe40bc00e4
limited-fears <49963808+limited-fears@users.noreply.github.com>
2025-07-11 19:45:05 -0400

    Delete xc2 directory

--- xc2/scripts/items_import.lua
+++
@@ -1,6 +0,0 @@
-
-Tracker:AddItems("items/items.json")
-Tracker:AddItems("items/hint_items.json")
-Tracker:AddItems("items/location_items.json")
-Tracker:AddItems("items/labels.json")
-                
