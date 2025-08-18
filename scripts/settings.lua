2937063852e7d99bfca1225f111b009222e9b012
limited-fears <49963808+limited-fears@users.noreply.github.com>
2025-08-19 00:21:56 +0100

    more

--- scripts/settings.lua
+++ scripts/settings.lua
@@ -4,8 +4,3 @@
 ------------------------------------------------------------------
 AUTOTRACKER_ENABLE_ITEM_TRACKING = true
 AUTOTRACKER_ENABLE_LOCATION_TRACKING = true
-{
-     "smooth_scaling": true
-    "smooth_map_scaling": true
-    "allow_deferred_logic_update": true
-}
