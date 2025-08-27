f17bda0fec0c1aa958600e39c0762cb9d6ce3553
Rayze421 <michaelsschmuck@gmail.com>
2025-08-27 02:13:35 -0400

    added the missing NLA json references to the init

--- scripts/init.lua
+++ scripts/init.lua
@@ -38,6 +38,8 @@ if not IS_ITEMS_ONLY then -- <--- use variant info to optimize loading
     Tracker:AddLocations("locations/NLA/barracks.jsonc")
     Tracker:AddLocations("locations/NLA/manonship.jsonc")
     Tracker:AddLocations("locations/NLA/industrial.jsonc")
+    Tracker:AddLocations("locations/NLA/commercial.jsonc")
+    Tracker:AddLocations("locations/NLA/residential.jsonc")
     Tracker:AddLocations("locations/cauldros.jsonc")
     Tracker:AddLocations("locations/nla.jsonc")
     Tracker:AddLocations("locations/noctilum.jsonc")
